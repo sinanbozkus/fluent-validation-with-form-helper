e4751741fd7d4d917b012dff918209f8f3f37995
Sinan Bozkuş <sinan.bozkus@gmail.com>
2019-05-12 16:30:41 +0300

    Added project items.

---
+++ FluentValidationSample/Startup.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using FluentValidation;
+using FluentValidation.AspNetCore;
+using FluentValidationSample.Models;
+using FluentValidationSample.Validators;
+using FormHelper;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace FluentValidationSample
+{
+    public class Startup
+    {
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
+        public IConfiguration Configuration { get; }
+
+        // This method gets called by the runtime. Use this method to add services to the container.
+        // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddFormHelper(new FormHelperConfiguration
+            {
+                CheckTheFormFieldsMessage = "Form alanlarını kontrol ediniz."
+            });
+
+            services.AddMvc()
+                .AddFluentValidation()
+                .SetCompatibilityVersion(CompatibilityVersion.Version_2_2);
+
+            services.AddTransient<IValidator<StudentViewModel>, StudentViewModelValidator>();
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IHostingEnvironment env)
+        {
+            app.UseDeveloperExceptionPage();
+            app.UseStaticFiles();
+
+            app.UseMvc(routes =>
+            {
+                routes.MapRoute(
+                    name: "default",
+                    template: "{controller=Home}/{action=Index}/{id?}");
+            });
+        }
+    }
+}
---
+++ FluentValidationSample/Validators/StudentViewModelValidator.cs
@@ -0,0 +1,36 @@
+﻿using FluentValidation;
+using FluentValidationSample.Models;
+using System.Linq;
+
+namespace FluentValidationSample.Validators
+{
+    public class StudentViewModelValidator : AbstractValidator<StudentViewModel>
+    {
+        public StudentViewModelValidator()
+        {
+            RuleFor(x => x.StudentName)
+                .NotEmpty()
+                .Length(4, 50);
+
+            RuleFor(x => x.Email)
+                .EmailAddress();
+
+            RuleFor(x => x.StudentNumber)
+                .NotEmpty()
+                .Length(6, 8)
+                .Custom((studentNumber, context) =>
+                {
+                    var arr = new[] { "A", "B", "C" };
+
+                    if (!arr.Contains(studentNumber.Substring(0, 1)))
+                    {
+                        context.AddFailure("Student number must start with 'A', 'B' or 'C'.");
+                    }
+                });
+
+            RuleFor(x => x.Credit)
+                .InclusiveBetween(0, 100)
+                .GreaterThanOrEqualTo(70).When(x => x.Graduated == true);
+        }
+    }
+}
